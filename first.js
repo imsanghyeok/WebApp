b43c623c69f1e5fa7e118bf157ee03db715c12a3
imsanghyeok <wans2006@naver.com>
2019-02-25 09:44:09 +0900

    Create first.js

---
+++ first.js
@@ -0,0 +1,71 @@
+var a;
+let b = 20;
+
+console.log("hello, world");
+console.log("a = " + a);
+console.log("b = " + b);
+console.log(a+b);
+
+a = 30;
+console.log(a);
+
+const x = 10;
+console.log("x = " + x);
+
+//x=100;
+console.log("x = " + x);
+
+console.log("=====================================================");
+
+var count = 10;
+var blue = 0x0000ff;
+var umask = 0o0022;
+var roomTemp = 21.5;
+var c = 3.0e6;
+var e = -1.6e-19;
+var inf = Infinity;
+var ninf = -Infinity;
+var nan = NaN;
+
+console.log(count);
+console.log(blue);
+console.log(umask);
+console.log(roomTemp);
+console.log(c);
+console.log(e);
+console.log(inf);
+console.log(ninf);
+console.log(nan);
+
+console.log("=====================================================");
+
+var small = Number.EPSILON;
+var bigInt = Number.MAX_SAFE_INTEGER;
+var max = Number.MAX_VALUE;
+var minInt = Number. MIN_SAFE_INTEGER;
+var min = Number.MIN_VALUE;
+var nInf = Number.NEGATIVE_INFINITY;
+var nan2 = Number.NaN;
+var inf2 = Number.POSITIVE_INFINITY;
+
+console.log(small);
+console.log(bigInt);
+console.log(max);
+console.log(minInt);
+console.log(min);
+console.log(nInf);
+console.log(nan2);
+console.log(inf2);
+
+console.log("=====================================================");
+
+var dialog = 'sam looked up, and said "hello, old friend", as Max walked in.';
+var imperative = "Don't do that";
+console.log(dialog);
+console.log(imperative);
+
+var dialog1 = "He looked up and said \" don't do that!\" to Max";
+var dialog2 = 'He looked up and said "don\'t do that!" to Max';
+console.log(dialog1);
+console.log(dialog2);
+console.log("Hello, \nWorld!!!");
