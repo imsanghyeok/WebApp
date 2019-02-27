c243a97c4c14403040e2d8b8187b0b1741805cf2
imsanghyeok <wans2006@naver.com>
2019-02-27 18:16:59 +0900

    Update fourth.js

--- fourth.js
+++ fourth.js
@@ -309,3 +309,39 @@ console.log(cards.filter(c=>c.value === 10));
 console.log();
 console.log(cards.filter(c=>c.value > 10 && c.suit === 'H'));
 console.log("===============================================================");
+
+
+let arr21 = [5,7,2,4];
+let sum = arr21.reduce((a,x) => a += x, 0);
+// let sum == arr21.reduce((a+x)=> a+=x);
+
+let words = ["Beachball","Rodeo","Angel","Avrdvark",
+              "Xylophone","November","Chocolate"];
+let alphabetical = words.reduce((a,x) => {
+  if (!a[x[0]]) a[x[0]] = [];
+  a[x[0]].push(x);
+  return a;
+},{});
+console.log(words);
+console.log(alphabetical);
+
+console.log(arr21);
+console.log(sum);
+
+let longWords = words.reduce((a,w) => w.length>6 ? a+ " " + w : a, "").trim();
+console.log(longWords);
+console.log("================================================================");
+
+
+let data = [3.3, 5, 7.2, 12, 4, 6, 10.3];
+let stats = data.reduce( (a,x) => {
+    a.N++;
+    let delta = x - a.mean;
+    a.mean += delta / a.N;
+    return a;
+}, {N:0, mean : 0, M2 : 0});
+if(stats.N > 2){
+    stats.variance = stats.M2 / (stats.N - 1);
+    stats.stdev = Math.sqrt(stats.variance);
+}
+console.log(stats);
